88985dcea19787f9597a1bb6a26bee0a41a15d08
Chenxiao Guan <1011zaozao@gmail.com>
2018-09-21 17:42:04 -0400

    argument parser work in progress

--- Langevin-Dynamics-Simulator/langevin_dynamics_simulator.py
+++ Langevin-Dynamics-Simulator/langevin_dynamics_simulator.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import argparse
+
+def parse_args(args):
+    """
+    An parsing argument function that allows the program
+    to read input from the user.
+
+    Args:
+        args: Unparsed argument variable.
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init_pos', \
+    help='The initial position of the particle')
+    parser.add_argument('--velocity', \)
